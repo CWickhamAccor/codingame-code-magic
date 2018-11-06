1632a7ce57cc8f54eb628dfb7c0e5e2e05ce2d8b
charles <charles.wickham@consulting-for.accor.com>
2018-11-06 13:52:14 +0100

    several draft opti (curve + picks) ward handling with items some memoization optimizations

--- src/mcts/newMain.js
+++ src/mcts/newMain.js
@@ -16,12 +16,12 @@ const scale = {
     4: 0,
     5: 2.5,
     6: 2,
-    7: 2,
+    7: 4,
     8: 2,
     9: 3,
     10: 0.75,
     11: 1.5,
-    12: 2,
+    12: 2.5,
     13: 2.5,
     14: 0,
     15: 2,
@@ -73,11 +73,11 @@ const scale = {
     61: 2,
     62: 2.5,
     63: 2.5,
-    64: 3,
-    65: 3,
+    64: 3.5,
+    65: 4,
     66: 2,
-    67: 3.5,
-    68: 3,
+    67: 4.5,
+    68: 4,
     69: 4.5,
     70: 2.5,
     71: 1.5,
@@ -91,12 +91,12 @@ const scale = {
     79: 0.75,
     80: 4.5,
     81: 2,
-    82: 2.5,
+    82: 3.5,
     83: 0,
     84: 3,
     85: 2.5,
     86: 2,
-    87: 2.5,
+    87: 3.5,
     88: 2,
     89: 2,
     90: 0.75,
@@ -118,14 +118,14 @@ const scale = {
     106: 2.5,
     107: 2,
     108: 1,
-    109: 1.5,
+    109: 2,
     110: 0.75,
     111: 3,
     112: 2,
     113: 0.5,
     114: 2,
     115: 2.5,
-    116: 3,
+    116: 5,
     120: 3,
     122: 2.5,
     123: 2.5,
@@ -133,15 +133,22 @@ const scale = {
     133: 2.5,
     135: 3.5,
     139: 5,
-    141: 2.5,
+    141: 2,
     144: 4,
+    148: 3,
     150: 4,
     151: 5,
     152: 3.5,
-    155: 3.5,
+    155: 2.5,
     158: 5,
     159: 2,
 };
+const curve = [
+    { low: 0, high: 2, count: 10 },
+    { low: 3, high: 5, count: 10 },
+    { low: 6, high: 8, count: 6 },
+    { low: 9, high: 12, count: 4 },
+];
 
 /** ************************************************* */
 /**                tools functions                    */
@@ -318,8 +325,8 @@ function pass() {
 function performActions() {
     turnActions.push(...bestTurn.actions.map((action) => {
         let string = '';
-        if (action.type === 'summon') {string += `SUMMON ${action.source}`; }
-        if (action.type === 'use') {string += `USE ${action.source} ${action.target}`; }
+        if (action.type === 'summon') { string += `SUMMON ${action.source}`; }
+        if (action.type === 'use') { string += `USE ${action.source} ${action.target}`; }
         if (action.type === 'attack') { string += `ATTACK ${action.source} ${action.target}`; }
         return string;
     }));
@@ -334,7 +341,7 @@ function act() {
 /**                 MCTS functions                    */
 /** ************************************************* */
 
-/** **** **** Play creature */
+/** **** **** Use Items */
 
 function useItem(game, source, target) {
     game.player.mana -= source.ccm;
@@ -343,7 +350,7 @@ function useItem(game, source, target) {
     ['charge', 'breakthrough', 'drain', 'lethal', 'guard', 'ward'].forEach((ability) => {
         target[ability] = source[ability] ? source.type === 'greenItem' : target[ability];
     });
-    if (['redItem', 'blueItem'].includes(source.type)){
+    if (['redItem', 'blueItem'].includes(source.type)) {
         if (target.ward) {
             // cancel damage
             target.ward = false;
@@ -353,14 +360,6 @@ function useItem(game, source, target) {
             splice(game.oppBoard, target.id);
         }
     }
-    // target.charge = source.charge ? source.type === 'greenItem' : target.charge;
-    // target.charge = source.charge ? source.type === 'greenItem' : target.charge;
-    // if (source.charge) { target.charge = source.type === 'greenItem'; }
-    // if (source.breakthrough) { target.breakthrough = source.type === 'greenItem'; }
-    // if (source.drain) { target.drain = source.type === 'greenItem'; }
-    // if (source.lethal) { target.lethal = source.type === 'greenItem'; }
-    // if (source.guard) { target.guard = source.type === 'greenItem'; }
-    // if (source.ward) { target.ward = source.type === 'greenItem'; }
     splice(game.hand, source.id);
 }
 
@@ -379,6 +378,8 @@ function playCreature(game, source) {
 function attack(myBoard, source, target) {
     if (canKill(source, target)) {
         splice(myBoard, target.id);
+    } else if (target.ward && source.power > 0) {
+        target.ward = false;
     } else {
         target.toughness -= source.power;
     }
@@ -431,13 +432,13 @@ function getUpdatedGameState(game, { type, source, target }) {
 /** **** **** Plays */
 
 function getPossiblePlays(game) {
-    const { hand, player, myBoard, oppBoard } = game;
-    // @TODO find all possible targets for spells
-    if (myBoard.length === 6) { return []; }
+    const {
+        hand, player, myBoard, oppBoard,
+    } = game;
     const castableCards = hand.filter(card => card.ccm <= player.mana);
     const plays = [];
     castableCards.forEach((card) => {
-        if (card.type === 'creature') {
+        if (card.type === 'creature' && myBoard.length < 6) {
             plays.push({
                 type: 'summon',
                 source: card.id,
@@ -466,14 +467,13 @@ function getPossiblePlays(game) {
 function getPossibleAttacks(game) {
     const { myBoard, oppBoard } = game;
     const key = JSON.stringify({ myBoard, oppBoard });
-    if (memoHelper.gameState.has(key)) {
-        debug('(attacks) memo used !');
-        return copy(memoHelper.gameState.get(key));
+    if (memoHelper.actions.attacks.has(key)) {
+        // debug('(attacks) memo used !');
+        return copy(memoHelper.actions.attacks.get(key));
     }
     const attacks = [];
-    // @TODO add face attacks
     const oppGuards = oppBoard.filter(crea => crea.guard);
-    myBoard.filter(crea => !crea.sick && !crea.attacked)
+    myBoard.filter(crea => !crea.sick && !crea.attacked && crea.power > 0)
         .forEach((crea) => {
             if (oppGuards.length > 0) {
                 oppGuards.forEach((opp) => {
@@ -498,6 +498,7 @@ function getPossibleAttacks(game) {
                 });
             }
         });
+    memoHelper.actions.attacks.set(key, copy(attacks));
     return attacks;
 }
 
@@ -515,8 +516,8 @@ function getPossibleActions(game) {
     //     return memoHelper.actions.get(key);
     // }
     const actions = [];
-    actions.push(...getPossiblePlays(game));
     actions.push(...getPossibleAttacks(game));
+    actions.push(...getPossiblePlays(game));
     // memoHelper.actions.set(key, actions);
     return actions;
 }
@@ -531,10 +532,7 @@ function getSetOfPossibleActions(game, set = [], actual = []) {
         return;
     }
     const actions = getPossibleActions(game);
-    // printObj('actions', actions);
     if (actions.length === 0) {
-        // set.push(actual);
-        // const score = getScore(game);
         return;
     }
 
@@ -547,6 +545,8 @@ function getSetOfPossibleActions(game, set = [], actual = []) {
         const newGameState = getUpdatedGameState(game, action);
         const newScore = getScore(newGameState);
         newActual.push(action);
+        // printObj('turn', newActual);
+        // debug(`score: ${newScore}`);
         if (newScore > bestTurn.score) {
             bestTurn.actions = newActual;
             bestTurn.score = newScore;
@@ -565,10 +565,10 @@ function play(gameState) {
     const t0 = performance.now();
     getSetOfPossibleActions(gameState, set);
     const t1 = performance.now();
-    debug(`possibleSets : ${Math.round((t1 - t0))} milliseconds.`);
+    // debug(`possibleSets : ${Math.round((t1 - t0))} milliseconds.`);
     // printObj('Actions', set);
-    debug(`chosen between ${set.length} turns`);
-    printObj('bestTurn', bestTurn);
+    // debug(`chosen between ${set.length} turns`);
+    // printObj('bestTurn', bestTurn);
     performActions();
     pass();
     return set;
@@ -586,14 +586,18 @@ function draft({ player }, cards) {
     cards.forEach((card) => {
         if (!Object.keys(scale).includes(card.number.toString())) {
             debug(`card ${card.number} not in the scale`);
-            card.score = -1;
+            card.score = -Infinity;
         } else {
-            card.score = scale[card.number]; // + curve[card.ccm] * player.deck / 60;
+            card.score = scale[card.number];
+            const curveElem = curve.find(elem => card.ccm >= elem.low && card.ccm <= elem.high);
+            card.score += curveElem.count * player.deck / 30;
             debug('score : ', card.score);
         }
     });
     cards.sort((card1, card2) => card2.score - card1.score);
-    // curve[cards[0].ccm]--;
+    const curvePicked = curve.find(elem => cards[0].ccm >= elem.low && cards[0].ccm <= elem.high);
+    curvePicked.count -= 1;
+
 
     pick(cards[0].draftId);
 }
